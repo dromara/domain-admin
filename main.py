a7a4d7143dafdedf6cd4246ec659b4c8cbd5273a
pengshiyu <1940607002@qq.com>
2022-09-23 17:39:10 +0800

    fix

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from domain_admin.app import app
 
 if __name__ == '__main__':
     app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.run(port=8001, host="0.0.0.0")
+    app.run(port=8080, host="0.0.0.0")
