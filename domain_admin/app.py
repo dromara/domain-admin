9fb319c19c6e884489dd217374dbf0e8d005edbe
pengshiyu <1940607002@qq.com>
2022-09-28 10:24:57 +0800

    fix

--- domain_admin/app.py
+++ domain_admin/app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import request, make_response
+from flask import request, make_response, send_file
 from flask_cors import CORS
 
 from domain_admin.model import db
@@ -7,7 +7,7 @@ from domain_admin.router import routes
 from domain_admin.utils.flask_ext.flask_app import FlaskApp
 from domain_admin.utils.flask_ext.register import register_app_routers, register_error_handler
 
-app = FlaskApp(__name__, static_folder=None)
+app = FlaskApp(__name__, static_folder="public", static_url_path="/")
 
 CORS(app, supports_credentials=True)
 
@@ -31,3 +31,8 @@ def before_request():
 def teardown_request(exc):
     if not db.is_closed():
         db.close()
+
+
+@app.get('/')
+def index():
+    return send_file('public/index.html')
---
+++ domain_admin/public/electron-preload.js
@@ -0,0 +1,1 @@
+(()=>{var e={};module.exports=e})();
