4205d0740fd7c7f2c75a16d1d1ede713c7fa6343
pengshiyu <1940607002@qq.com>
2023-06-04 19:37:23 +0800

    fix

--- tests/utils/test_cert_util.py
+++ tests/utils/test_cert_util.py
@@ -6,8 +6,17 @@
 """
 
 from domain_admin.utils import cert_util
+from domain_admin.utils.cert_util import cert_socket_v2
 
 
 def test_get_cert_info():
     # print(cert_util.get_cert_info('dfyun-spare1.showdoc.com.cn:8888'))
     print(cert_util.get_cert_info('www.baidu.com'))
+
+
+def test_cert_socket_v2():
+    ret = cert_socket_v2.get_ssl_cert_info(
+        'www.csdn.net',
+        '123.129.227.79'
+    )
+    print(ret)
