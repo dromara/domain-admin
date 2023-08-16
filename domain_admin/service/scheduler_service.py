06f3ffbfdac5c95d99d91b8a2f95c387b80cc676
pengshiyu <1940607002@qq.com>
2023-08-16 10:22:45 +0800

    修复 cron表达式星期参数的bug

--- domain_admin/service/scheduler_service.py
+++ domain_admin/service/scheduler_service.py
@@ -48,7 +48,6 @@ JOB_DEFAULTS = {
 
 scheduler = BackgroundScheduler(job_defaults=JOB_DEFAULTS)
 
-
 # 任务列表
 TASK_LIST = [
     # 更新证书信息
@@ -63,6 +62,32 @@ TASK_LIST = [
     # 触发通知
     notify_service.notify_all_event,
 ]
+
+
+def crontab_compatible_weekday(expr):
+    """
+    :param expr:
+    :return:
+
+    bugfix: 0-6表示周一到周日，改为周日到周六，并支持7为周日
+    day_of_week: number or name of weekday (0-6 or mon,tue,wed,thu,fri,sat,sun)
+    ref: https://github.com/agronholm/apscheduler/issues/286#issuecomment-449273964
+    """
+    if expr == "*":
+        return expr
+
+    mapping = {
+        "0": "sun",
+        "1": "mon",
+        "2": "tue",
+        "3": "wed",
+        "4": "thu",
+        "5": "fri",
+        "6": "sat",
+        "7": "sun"
+    }
+
+    return "".join(map(lambda x: mapping.get(x, x), expr))
 
 
 def init_scheduler():
@@ -90,7 +115,7 @@ def update_job(cron_exp):
         hour=hour,
         day=day,
         month=month,
-        day_of_week=day_of_week
+        day_of_week=crontab_compatible_weekday(day_of_week)
     )
 
 
