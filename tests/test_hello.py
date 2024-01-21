ba5a70be67b74097e7073a97197846e26dc6dfd4
pengshiyu <1940607002@qq.com>
2024-01-21 14:56:43 +0800

    fix

---
+++ tests/test_hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : test_hello.py
+@Date    : 2024-01-21
+@Author  : Peng Shiyu
+"""
+import unittest
+
+
+class HelloTest(unittest.TestCase):
+    def test_hi(self):
+        assert 'hi' == 'hi'
